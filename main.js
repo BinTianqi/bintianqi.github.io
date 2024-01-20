2c557ea021a8c2fea595122022d59afec5e6463c
BinTianqi <1220958406@qq.com>
2024-01-20 21:20:09 +0800

    add more description to android owner

---
+++ main.js
@@ -0,0 +1,9 @@
+function switchToAndroidOwner(){
+    var myIframe = document.getElementById("mainIframe");
+    myIframe.src = "AndroidOwner.html";
+}
+
+function switchToHomePage(){
+    var myIframe = document.getElementById("mainIframe");
+    myIframe.src = "introduction.html";
+}
