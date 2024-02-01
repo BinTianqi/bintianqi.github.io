626a97ee788b0e8fd5893e22855ce6f1b730331a
BinTianqi <1220958406@qq.com>
2024-02-01 19:29:34 +0800

    Split AndroidOwner.html

--- main.js
+++
@@ -1,9 +0,0 @@
-function switchToAndroidOwner(){
-    var myIframe = document.getElementById("mainIframe");
-    myIframe.src = "AndroidOwner.html";
-}
-
-function switchToHomePage(){
-    var myIframe = document.getElementById("mainIframe");
-    myIframe.src = "introduction.html";
-}
